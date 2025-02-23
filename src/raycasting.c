d10ef01a78543f60da76f7df20b48d3b3b0a5a47
niida <54788301+naokiiida@users.noreply.github.com>
2025-02-23 12:20:51 +0900

    refactor ray_init

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,14 +6,14 @@
 /*   By: naokiiida <naokiiida@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:53:00 by naokiiida         #+#    #+#             */
-/*   Updated: 2025/02/22 21:35:43 by niida            ###   ########.fr       */
+/*   Updated: 2025/02/23 11:54:51 by niida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 // カメラ平面上の位置を計算するための正規化用変数の生成関数
-double	calculate_camera_x(int x)
+static double	calculate_camera_x(int x)
 {
 	double	camera_x;
 
@@ -22,33 +22,29 @@ double	calculate_camera_x(int x)
 }
 
 // レイの方向を計算する関数
-t_vector2d	calculate_ray_dir(t_vector2d dir, t_vector2d plane, double cameraX)
+static t_vector2d	calculate_ray_dir(t_vector2d dir, t_vector2d plane, double cameraX)
 {
-	t_vector2d	ray_dir;
-
-	ray_dir.x = dir.x + plane.x * cameraX;
-	ray_dir.y = dir.y + plane.y * cameraX;
-	return (ray_dir);
+	return (vector_add(dir, vector_scale(plane, cameraX)));
 }
 
 // レイの移動量を計算する関数
-t_vector2d	calculate_delta_dist(t_vector2d rayDir)
+static t_vector2d	calculate_delta_dist(t_vector2d ray_dir)
 {
 	t_vector2d	delta_dist;
 
-	if (rayDir.x == 0)
+	if (ray_dir.x == 0)
 		delta_dist.x = 1e30;
 	else
-		delta_dist.x = fabs(1 / rayDir.x);
-	if (rayDir.y == 0)
+		delta_dist.x = fabs(1 / ray_dir.x);
+	if (ray_dir.y == 0)
 		delta_dist.y = 1e30;
 	else
-		delta_dist.y = fabs(1 / rayDir.y);
+		delta_dist.y = fabs(1 / ray_dir.y);
 	return (delta_dist);
 }
 
 // rayの移動量の初期化と始めの整数座標までの値を求めray->side_distに代入
-void	calculate_step_and_side_dist(t_ray *ray, t_player *player)
+static void	calculate_step_and_side_dist(t_ray *ray, t_player *player)
 {
 	if (ray->dir.x < 0)
 	{
@@ -72,6 +68,25 @@ void	calculate_step_and_side_dist(t_ray *ray, t_player *player)
 		ray->side_dist.y = (ray->grid.y + 1.0 - player->pos.y)
 			* ray->delta_dist.y;
 	}
+}
+
+// rayの初期化
+void	init_ray(t_vars *vars, int x)
+{
+	double		camera_x;
+	t_vector2d	ray_dir;
+
+	// カメラ平面上の位置を計算
+	camera_x = calculate_camera_x(x);
+	// rayの方向を計算
+	ray_dir = calculate_ray_dir(vars->player->dir, vars->player->plane, camera_x);
+	// マップ上の初期座標を取得
+	vars->ray->grid.x = (int)vars->player->pos.x;
+	vars->ray->grid.y = (int)vars->player->pos.y;
+	// rayの方向によって、x,y方向の移動量を計算
+	vars->ray->delta_dist = calculate_delta_dist(ray_dir);
+	// 始めの整数座標までの値を求め、ray->side_distに代入
+	calculate_step_and_side_dist(vars->ray, vars->player);
 }
 
 // 壁に当たったか判定
@@ -101,25 +116,6 @@ void	perform_dda(t_ray *ray, int map[ROWS][COLS])
 			ray->side = Y_AXIS;
 		}
 	}
-}
-
-// rayの初期化
-static void	init_ray(t_vars *vars, int x)
-{
-	double		camera_x;
-	t_vector2d	ray_dir;
-
-	// カメラ平面上の位置を計算
-	camera_x = calculate_camera_x(x);
-	// rayの方向を計算
-	ray_dir = calculate_ray_dir(vars->player->dir, vars->player->plane, camera_x);
-	// マップ上の初期座標を取得
-	vars->ray->grid.x = (int)vars->player->pos.x;
-	vars->ray->grid.y = (int)vars->player->pos.y;
-	// rayの方向によって、x,y方向の移動量を計算
-	vars->ray->delta_dist = calculate_delta_dist(ray_dir);
-	// 始めの整数座標までの値を求め、ray->side_distに代入
-	calculate_step_and_side_dist(vars->ray, vars->player);
 }
 
 // 壁までの距離を計算
