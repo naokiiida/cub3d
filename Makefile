1795343b83d279739acb6fa0391f1d3bbcb847fe
niida <54788301+naokiiida@users.noreply.github.com>
2025-02-23 20:12:40 +0900

    split refactoring.c headers

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ endif
 
 LIBFT := libft/
 GNL := get_next_line/
-SRCS := main.c init.c raycasting.c draw.c move.c vector.c close.c
+SRCS := main.c init.c ray_init.c ray_to_wall.c raycast.c draw.c move.c vector.c close.c
 OBJS := $(SRCS:%.c=$(ODIR)%.o)
 DEPS = $(patsubst %.o,%.d, $(OBJS))
 DEPFLAGS := -MMD -MP
--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: naokiiida <naokiiida@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 21:47:57 by naokiiida         #+#    #+#             */
-/*   Updated: 2025/02/23 17:21:17 by niida            ###   ########.fr       */
+/*   Updated: 2025/02/23 20:03:54 by niida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,21 @@
 # include "mlx.h"
 # include <assert.h>
 
-enum e_x_events
+enum			e_x_events
 {
 	X_EVENT_KEY_PRESS = 2,
 	X_EVENT_KEY_RELEASE = 3,
 	X_EVENT_KEY_EXIT = 17
 };
 
-enum e_x_masks
+enum			e_x_masks
 {
 	X_KEY_PRESS_MASK = 1L << 0,
 	X_KEY_RELEASE_MASK = 1L << 1,
 	X_STRUCTURE_NOTIFY_MASK = 1L << 17,
 };
 
-enum e_keys
+enum			e_keys
 {
 	KEY_ESC = 53,
 	KEY_Q = 12,
@@ -58,7 +58,7 @@ enum e_keys
 // # define COLS 15
 # define WIDTH 640
 # define HEIGHT 480
-//has to be power of 2 for & (TEXTURE_HEIGHT - 1) wrap around to work
+// has to be power of 2 for & (TEXTURE_HEIGHT - 1) wrap around to work
 # define TEXTURE_WIDTH 64
 # define TEXTURE_HEIGHT 64
 
@@ -66,7 +66,7 @@ enum e_keys
 // # define WALL_STRIP_WIDTH 1
 // # define NUM_RAYS WIDTH / WALL_STRIP_WIDTH
 
-enum e_map
+enum			e_map
 {
 	MAP_WALL = 1,
 	MAP_BLANK = 0
@@ -133,16 +133,16 @@ typedef struct s_texture
 
 typedef struct s_img
 {
-	void	*img;
-	char	*addr; //int *data
+	void		*img;
+	char		*addr;
 
-	int		*data;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	int		img_width;
-	int		img_height;
-}	t_img;
+	int			*data;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			img_width;
+	int			img_height;
+}				t_img;
 
 typedef struct s_vars
 {
@@ -158,15 +158,20 @@ typedef struct s_vars
 
 void			init(t_vars *vars);
 int				raycasting(t_vars *vars);
-void draw_buffer(t_vars *vars, int x);
-void	draw(t_vars *vars);
-int	close_display(t_vars *vars);
-int	close_win(t_vars *vars);
-int key_press(int key, t_vars *vars);
-int key_release(int key, t_vars *vars);
-void	buffer_mlx_pixel_put(t_img *buffer, int x, int y, int color);
-void draw_map(t_vars *vars);
-t_vector2d	vector_add(t_vector2d v1, t_vector2d v2);
-t_vector2d	vector_scale(t_vector2d v, double scale);
-t_vector2d	vector_rotate(t_vector2d v, double angle);
+void			draw_buffer(t_vars *vars, int x);
+void			draw(t_vars *vars);
+int				close_display(t_vars *vars);
+int				close_win(t_vars *vars);
+int				key_press(int key, t_vars *vars);
+int				key_release(int key, t_vars *vars);
+void			buffer_mlx_pixel_put(t_img *buffer, int x, int y, int color);
+void			draw_map(t_vars *vars);
+t_vector2d		vector_add(t_vector2d v1, t_vector2d v2);
+t_vector2d		vector_scale(t_vector2d v, double scale);
+t_vector2d		vector_rotate(t_vector2d v, double angle);
+void			init_ray(t_vars *vars, int x);
+void			calculate_perp_wall_dist(t_ray *ray);
+void			calculate_wall_size(t_ray *ray, t_texture *texture);
+void			decide_draw_texture(t_ray *ray, t_texture *texture);
+int				get_texture_x(t_ray *ray, t_player *player);
 #endif
