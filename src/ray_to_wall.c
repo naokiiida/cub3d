8fe242da8daf5b9812e541f88977ba9920d03781
niida <54788301+naokiiida@users.noreply.github.com>
2025-02-23 19:55:00 +0900

    split raycasting file to ray_to_wall.c

--- src/ray_to_wall.c
+++ src/ray_to_wall.c
@@ -1,121 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   raycasting.c                                       :+:      :+:    :+:   */
+/*   ray_to_wall.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: naokiiida <naokiiida@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:53:00 by naokiiida         #+#    #+#             */
-/*   Updated: 2025/02/23 18:31:07 by niida            ###   ########.fr       */
+/*   Updated: 2025/02/23 19:54:48 by niida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// カメラ平面上の位置を計算するための正規化用変数の生成関数
-static double	calculate_camera_x(int x)
-{
-	double	camera_x;
-
-	camera_x = 2.0 * x / (double)WIDTH - 1;
-	return (camera_x);
-}
-
-// レイの方向を計算する関数
-static t_vector2d	calculate_ray_dir(t_vector2d dir, t_vector2d plane, double cameraX)
-{
-	return (vector_add(dir, vector_scale(plane, cameraX)));
-}
-
-// レイの移動量を計算する関数
-static t_vector2d	calculate_delta_dist(t_vector2d ray_dir)
-{
-	t_vector2d	delta_dist;
-
-	if (ray_dir.x == 0)
-		delta_dist.x = 1e30;
-	else
-		delta_dist.x = fabs(1 / ray_dir.x);
-	if (ray_dir.y == 0)
-		delta_dist.y = 1e30;
-	else
-		delta_dist.y = fabs(1 / ray_dir.y);
-	return (delta_dist);
-}
-
-// rayの移動量の初期化と始めの整数座標までの値を求めray->side_distに代入
-static void	calculate_step_and_side_dist(t_ray *ray, t_player *player)
-{
-	if (ray->dir.x < 0)
-	{
-		ray->step.x = -1;
-		ray->side_dist.x = (player->pos.x - ray->grid.x) * ray->delta_dist.x;
-	}
-	else
-	{
-		ray->step.x = 1;
-		ray->side_dist.x = (ray->grid.x + 1.0 - player->pos.x)
-			* ray->delta_dist.x;
-	}
-	if (ray->dir.y < 0)
-	{
-		ray->step.y = -1;
-		ray->side_dist.y = (player->pos.y - ray->grid.y) * ray->delta_dist.y;
-	}
-	else
-	{
-		ray->step.y = 1;
-		ray->side_dist.y = (ray->grid.y + 1.0 - player->pos.y)
-			* ray->delta_dist.y;
-	}
-}
-
-// rayの初期化
-void	init_ray(t_vars *vars, int x)
-{
-	double		camera_x;
-
-	// カメラ平面上の位置を計算
-	camera_x = calculate_camera_x(x);
-	// rayの方向を計算
-	vars->ray->dir = calculate_ray_dir(vars->player->dir, vars->player->plane, camera_x);
-	// マップ上の初期座標を取得 (マップの行へプレーヤーのxを当ててる)
-	vars->ray->grid.x = (int)vars->player->pos.x;
-	vars->ray->grid.y = (int)vars->player->pos.y;
-	// rayの方向によって、x,y方向の移動量を計算
-	vars->ray->delta_dist = calculate_delta_dist(vars->ray->dir);
-	// 始めの整数座標までの値を求め、ray->side_distに代入
-	calculate_step_and_side_dist(vars->ray, vars->player);
-}
-
-// 壁に当たったか判定
-static bool	is_hit_wall(int map[ROWS][COLS], t_grid grid)
-{
-	if (map[grid.x][grid.y] != MAP_BLANK)
-		return (true);
-	return (false);
-}
-
-// DDAアルゴリズムによる壁の衝突判定
-void	perform_dda(t_ray *ray, int map[ROWS][COLS])
-{
-	while (!is_hit_wall(map, ray->grid))
-	{
-		if (ray->side_dist.x < ray->side_dist.y)
-		{
-			ray->side_dist.x += ray->delta_dist.x;
-			// 現在地のrayの次の整数座標までの値を追加する
-			ray->grid.x += ray->step.x;
-			ray->side = X_AXIS;
-		}
-		else
-		{
-			ray->side_dist.y += ray->delta_dist.y;
-			ray->grid.y += ray->step.y;
-			ray->side = Y_AXIS;
-		}
-	}
-}
 
 // 壁までの距離を計算
 void	calculate_perp_wall_dist(t_ray *ray)
@@ -178,7 +73,7 @@ static double	get_wall_x(t_ray *ray, t_player *player)
 }
 
 // ヒットした壁のローカル座標を特定
-int get_texture_x(t_ray *ray, t_player *player)
+int	get_texture_x(t_ray *ray, t_player *player)
 {
 	int		tex_x;
 	double	wall_x;
@@ -190,34 +85,4 @@ int get_texture_x(t_ray *ray, t_player *player)
 	if (ray->side == Y_AXIS && ray->dir.y < 0)
 		tex_x = TEXTURE_WIDTH - tex_x - 1;
 	return (tex_x);
-}
-
-// フレームごとに描画を更新する関数
-int	raycasting(t_vars *vars)
-{
-	int	x;
-
-	x = -1;
-	// 描画バッファの初期化
-	// init(vars);
-	// x方向にrayを飛ばす
-	while (++x < WIDTH)
-	{
-		// rayの初期化
-		init_ray(vars, x);
-		// DDAによる壁の衝突判定
-		perform_dda(vars->ray, vars->map);
-		// 壁までの距離を計算
-		calculate_perp_wall_dist(vars->ray);
-		// 描画する壁の大きさの判定
-		calculate_wall_size(vars->ray, vars->texture);
-		// 壁のテクスチャ判定
-		decide_draw_texture(vars->ray, vars->texture);
-		// ヒットした壁のローカル座標を特定
-		vars->texture->tex_x = get_texture_x(vars->ray, vars->player);
-		// 描画バッファに壁を描画
-		draw_buffer(vars, x);
-	}
-	draw(vars);
-	return (0);
 }
