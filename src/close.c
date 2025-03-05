84ba73cbda868e0983461a3bcc588ddb13c214b8
niida <54788301+naokiiida@users.noreply.github.com>
2025-03-05 19:57:53 +0900

    fix: mlx_loop_end before free(mlx)

--- src/close.c
+++ src/close.c
@@ -25,40 +25,15 @@ int	err(char *function_name, const char *msg)
 
 #ifdef __APPLE__
 
-// int	close_win(t_vars *vars)
-// {
-// 	write(1, "closing...\n", 11);
-// 	mlx_destroy_image(vars->mlx, vars->buffer->img);
-// 	mlx_destroy_window(vars->mlx, vars->win);
-// 	free(vars->mlx);
-// 	vars->mlx = NULL;
-// 	free(vars->ray);
-// 	vars->ray = NULL;
-// 	free(vars->player);
-// 	vars->player = NULL;
-// 	free(vars->texture);
-// 	vars->texture = NULL;
-// 	free(vars->buffer);
-// 	vars->buffer = NULL;
-// 	exit(0);
-// }
-
-// int	close_display(t_vars *vars)
-// {
-// 	(void)vars;
-// 	write(1, "closing...\n", 11);
-// 	return (0);
-// }
-#elif __LINUX__
-
 int	close_win(t_vars *vars)
 {
+	write(1, "closing...\n", 11);
 	mlx_destroy_image(vars->mlx, vars->buffer->img);
 	mlx_destroy_window(vars->mlx, vars->win);
-	free(vars->map);
-	vars->map = NULL;
 	free(vars->mlx);
 	vars->mlx = NULL;
+	free(vars->map);
+	vars->map = NULL;
 	free(vars->ray);
 	vars->ray = NULL;
 	free(vars->player);
@@ -67,7 +42,34 @@ int	close_win(t_vars *vars)
 	vars->texture = NULL;
 	free(vars->buffer);
 	vars->buffer = NULL;
+	exit(0);
+}
+
+int	close_display(t_vars *vars)
+{
+	(void)vars;
+	write(1, "closing...\n", 11);
+	return (0);
+}
+#elif __LINUX__
+
+int	close_win(t_vars *vars)
+{
+	mlx_destroy_image(vars->mlx, vars->buffer->img);
+	mlx_destroy_window(vars->mlx, vars->win);
 	mlx_loop_end(vars->mlx);
+	free(vars->mlx);
+	vars->mlx = NULL;
+	free(vars->map);
+	vars->map = NULL;
+	free(vars->ray);
+	vars->ray = NULL;
+	free(vars->player);
+	vars->player = NULL;
+	free(vars->texture);
+	vars->texture = NULL;
+	free(vars->buffer);
+	vars->buffer = NULL;
 	return (0);
 }
 
